bc328169839f7d92a45fdb18bbaf7ac4be6642ef
Sebastian Kloppe <feedbackcalcest@gmail.com>
2018-08-15 20:03:35 +0200

    Refactoring

--- src/DataComp.tsx
+++ src/DataComp.tsx
@@ -2,35 +2,72 @@ import * as React from "react";
 import { fromJson, IData, ICategory, ILink } from "./DataFetcher";
 import "./DataComp.css";
 
+// INTERFACES
+
 interface IDataCompState {
-  loading: boolean;
+  isLoading: boolean;
   data: IData;
 }
 
+interface IMainListItemProps {
+  category: ICategory;
+}
+
+interface IChildListItemProps {
+  link: ILink;
+}
+
+// LOADING COMPONENT
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-text">loading...</div>
+      <div className="loading-spinner" />
+    </div>
+  );
+};
+
+// MAIN LIST COMPONENTS
+
+const MainList = (props: any) => {
+  return <ul className="list-main">{props.children}</ul>;
+};
+
+const MainListCardItem = (props: any) => {
+  return <div className="card">{props.children}</div>;
+};
+
+const MainListItem = (props: IMainListItemProps) => {
+  return <li className="list-main_item">{props.category.name}</li>;
+};
+
+// CHILD LIST COMPONENTS
+
+const ChildList = (props: any) => {
+  return <ul className="list-child">{props.children}</ul>;
+};
+
+const ChildListItem = (props: IChildListItemProps) => {
+  return (
+    <li className="list-child_item">
+      <a className="link" href={props.link.link} target="blank">
+        {props.link.link}
+      </a>
+    </li>
+  );
+};
+
 class DataComp extends React.Component<any, IDataCompState> {
-  private _getLinkList = (categoryId: number) => {
-    const subsetList: ILink[] = this.state.data.links.filter(
+  private _getLinkList = (categoryId: number): ILink[] =>
+    this.state.data.links.filter(
       (link: ILink) => link.categoryId === categoryId
     );
-    return (
-      <ul className="list-child">
-        {subsetList.map((link: ILink) => {
-          return (
-            <li className="list-child_item" key={link.id}>
-              <a className="link" href={link.link} target="blank">
-                {link.link}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  };
 
   constructor(props: any) {
     super(props);
     this.state = {
-      loading: true,
+      isLoading: true,
       data: {} as IData
     };
   }
@@ -39,7 +76,7 @@ class DataComp extends React.Component<any, IDataCompState> {
     try {
       const result: IData = await fromJson("./data/data.json?" + Date.now);
       this.setState({
-        loading: false,
+        isLoading: false,
         data: result
       });
     } catch (error) {
@@ -48,22 +85,21 @@ class DataComp extends React.Component<any, IDataCompState> {
   }
 
   render() {
-    return this.state.loading ? (
-      <div className="loading-container">
-        <div className="loading-text">loading...</div>
-        <div className="loading-spinner" />
-      </div>
+    return this.state.isLoading ? (
+      <Loading />
     ) : (
-      <ul className="list-main">
+      <MainList>
         {this.state.data.categories.map((category: ICategory) => (
-          <div className="card">
-            <li className="list-main_item" key={category.id}>
-              {category.name}
-            </li>
-            {this._getLinkList(category.id)}
-          </div>
+          <MainListCardItem key={category.id}>
+            <MainListItem category={category} />
+            <ChildList>
+              {this._getLinkList(category.id).map((link: ILink) => (
+                <ChildListItem key={link.id} link={link} />
+              ))}
+            </ChildList>
+          </MainListCardItem>
         ))}
-      </ul>
+      </MainList>
     );
   }
 }
