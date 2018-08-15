82bf2d15a6080e91a8aa881c9a4b50112b234e86
Sebastian Kloppe <feedbackcalcest@gmail.com>
2018-08-15 21:23:28 +0200

    deployment changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
 import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 render(<App dataSource="./data/data.json" />, document.getElementById("root"));
+registerServiceWorker();
