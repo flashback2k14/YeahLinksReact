c416213894d62abf5c65d7548d7e581575235fbb
Sebastian Kloppe <feedbackcalcest@gmail.com>
2018-08-18 16:00:45 +0200

    sort categories

--- src/DataFetcher.ts
+++ src/DataFetcher.ts
@@ -17,5 +17,12 @@ export interface IData {
 export const fromJson = async (source: string): Promise<IData> => {
   const response = await window.fetch(source);
   const responseData: IData = await response.json();
+  responseData.categories.sort((a: ICategory, b: ICategory) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+  });
   return responseData;
 };
