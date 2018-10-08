a46b5093519419c74206288b8c0d6afac1f47786
Sebastian Kloppe <feedbackcalcest@gmail.com>
2018-10-08 20:55:14 +0200

    CodeSandbox Github Changes Bug #3

--- src/components/atoms/SearchInput.tsx
+++ src/components/atoms/SearchInput.tsx
@@ -8,9 +8,10 @@ export const SearchInput = styled("input")`
   display: ${(props: ISearchInputProps) => (props.isVisible ? "flex" : "none")};
   flex: 1 0 auto;
 
-  margin-left: 2px;
-  margin-right: 12px;
-  margin-bottom: 4px;
+  margin: 2px 12px;
+  padding: 4px;
+
+  font-size: 1em;
 
   background: #004466;
   color: #00ffa2;
